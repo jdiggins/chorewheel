64f5c1ec593a1f9e413d1cbf60817bd9d7cabaf1
John Diggins <jdiggins@github.com>
2019-08-16 00:06:41 +0000

    initial commit

---
+++ trash-sms.py
@@ -0,0 +1,27 @@
+import os
+
+from twilio.rest import Client
+
+# Twilio Creds
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+# Roomie Info
+numbers = {
+        'John' : '+16076610544',
+        'Ingrid' : '+16263288581',
+        'Jack' : '+16039690268',
+        'Sean James' : '+19789952266'}
+
+for name, number in numbers.items():
+    message = client.messages \
+            .create(
+                    body="Hi " + name + " \U0001F601 Just your friendly reminder that it is (probably) trash day!",
+                    from_='+16178127055',
+                    to=number
+                    )
+    print(message.sid)
+
+
+
